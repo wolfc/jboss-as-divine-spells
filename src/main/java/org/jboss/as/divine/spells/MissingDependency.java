609b819b21dd1a2eb338773d51b37a5e681fc4b9
Carlo de Wolf <cdewolf@redhat.com>
2011-04-12 13:27:29 +0200

    Process failed services and show startExceptionMessage (if available MSC-84)

--- src/main/java/org/jboss/as/divine/spells/MissingDependency.java
+++ src/main/java/org/jboss/as/divine/spells/MissingDependency.java
@@ -14,6 +14,8 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -46,27 +48,39 @@ public class MissingDependency {
         final MBeanServerConnection connection = connector.getMBeanServerConnection();
         final ObjectName name = new ObjectName("jboss.msc:type=container,name=jboss-as");
         final CompositeData[] serviceStatuses = (CompositeData[]) connection.invoke(name, "queryServiceStatuses", null, null);
-        final Map<String, String> services = new HashMap<String, String>();
-        final Map<String, CompositeData> servicesMissingDependencies = new HashMap<String, CompositeData>();
+        final Map<String, CompositeData> services = new HashMap<String, CompositeData>();
+        final List<String> servicesMissingDependencies = new LinkedList<String>();
         for (final CompositeData serviceStatus : serviceStatuses) {
             final String serviceName = (String) serviceStatus.get("serviceName");
-            final String stateName = (String) serviceStatus.get("stateName");
-            services.put(serviceName, stateName);
+            services.put(serviceName, serviceStatus);
             final boolean dependencyMissing = (Boolean) serviceStatus.get("dependencyMissing");
             final boolean dependencyFailed = (Boolean) serviceStatus.get("dependencyFailed");
             if (dependencyMissing || dependencyFailed) {
-                servicesMissingDependencies.put(serviceName, serviceStatus);
+                servicesMissingDependencies.add(serviceName);
             }
         }
-        for (final Map.Entry<String, CompositeData> service : servicesMissingDependencies.entrySet()) {
-            System.out.print(service.getKey() + " -> ");
-            final String[] dependencies = (String[]) service.getValue().get("dependencies");
-            for (final String dependency : dependencies) {
-                String state = services.get(dependency);
-                if (state == null)
-                    state = "***MISSING***";
-                if (!state.equals("UP")) {
-                    System.out.print(" " + dependency + " (" + state + ")");
+        for (final String serviceName : servicesMissingDependencies) {
+            System.out.print(serviceName + " ");
+            final CompositeData service = services.get(serviceName);
+            final String myState = (String) service.get("stateName");
+            if (myState.equals("START_FAILED")) {
+                System.out.print("(START_FAILED)");
+                if (service.containsKey("startExceptionMessage"))
+                    System.out.print(": " + service.get("startExceptionMessage"));
+            }
+            else {
+                System.out.print("-> ");
+                final String[] dependencies = (String[]) service.get("dependencies");
+                for (final String dependency : dependencies) {
+                    String state;
+                    final CompositeData dependencyServiceState = services.get(dependency);
+                    if (dependencyServiceState == null)
+                        state = "***MISSING***";
+                    else
+                        state = (String) dependencyServiceState.get("stateName");
+                    if (!state.equals("UP")) {
+                        System.out.print(" " + dependency + " (" + state + ")");
+                    }
                 }
             }
             System.out.println();
